c794266f8e243c09e1c08fb3663bb22c98aa2ea4
stevetwalker <29964517+stevetwalker@users.noreply.github.com>
2020-05-03 21:10:43 -0700

    updated db.connect

--- main.py
+++ main.py
@@ -53,8 +53,8 @@ def next_turn(room):
     return render_template('next-turn.jinja2', room=session['room'])
 
 if __name__ == "__main__":
-    db.connect()
-    db.execute_sql('PRAGMA foreign_keys = ON;')
+    db.connect(os.environ.get('DATABASE_URL', 'sqlite:///my_database.db'))
+#    db.execute_sql('PRAGMA foreign_keys = ON;')
     db.create_tables([Charades])
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
